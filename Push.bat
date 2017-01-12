00683959ec1cf4e4f383362ac3f5e3af60cae7e8
Naigao.JIN <naigao.jin@gmail.com>
2017-01-13 00:11:54 +0800

    '2'

--- Push.bat
+++ Push.bat
@@ -7,7 +7,6 @@ echo\&echo
 
 git add -v .
 git commit -m '2'
-
 git push origin master
 
 echo\&echo done...
--- make.bat
+++ make.bat
@@ -12,3 +12,16 @@ del *.log
 del *.dvi
 del *.asv
 del *.aux
+
+@echo off
+@title windows git
+
+set path= %path%;C:\Users\Administrator\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd
+
+echo\&echo
+
+git add -v .
+git commit -m '2'
+git push origin master
+
+echo\&echo done...
