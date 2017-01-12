eb50a4c76e92b84257550a46415a417c663a86f7
Naigao.JIN <naigao.jin@gmail.com>
2017-01-13 00:28:44 +0800

    '2'

--- make.bat
+++ make.bat
@@ -1,3 +1,7 @@
+@taskkill /f /IM Acrobat.exe
+
+set path= %path%;C:\Users\Administrator\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd;C:\Program Files (x86)\Adobe\Acrobat 11.0\Acrobat
+
 del *.bak
 del *.sav
 del *.aux
@@ -12,16 +16,13 @@ del *.log
 del *.dvi
 del *.asv
 del *.aux
+Acrobat.exe   main.pdf
+
 
 @echo off
 @title windows git
-
-set path= %path%;C:\Users\Administrator\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd
-
 echo\&echo
-
 git add -v .
 git commit -m '2'
 git push origin master
-
 echo\&echo done...
